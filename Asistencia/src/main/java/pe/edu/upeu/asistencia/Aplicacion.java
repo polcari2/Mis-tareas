9b5d121256813b5e7791d4a5fdeb79606a3369c5
polcari2 <cariapazapaul@gmail.com>
2025-08-23 22:10:18 -0500

    ejemplo basico

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplicacion.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando aplicacion");
+
+        AsistenciaApplication.main(args);
+
+    }
+
+}
