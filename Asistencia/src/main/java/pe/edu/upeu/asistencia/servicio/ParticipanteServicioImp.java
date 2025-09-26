e1fcf1f44ffc84da4076862d6e9b8470b35fabf9
SebastianSumi <cspam7780@gmail.com>
2025-09-26 16:35:06 -0500

    ...

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/ParticipanteServicioImp.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/servicio/ParticipanteServicioImp.java
@@ -10,7 +10,7 @@ public class ParticipanteServicioImp extends ParticipanteRepository implements P
 
     @Override
     public void save(Participante participante) {
-        participantes.add(participante);
+        super.save(participante);
     }
 
     @Override
@@ -30,9 +30,6 @@ public class ParticipanteServicioImp extends ParticipanteRepository implements P
 
     @Override
     public List<Participante> findAll(){
-        if(participantes.isEmpty()){
-            return super.findAll();
-        }
-        return participantes;
+        return super.findAll();
     }
 }
