55f1d874ce339697f1cbccfa44fe1b8585a8f34c
SebastianSumi <cspam7780@gmail.com>
2025-10-06 12:47:03 -0500

    ..

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/model/UnidadMedida.java
@@ -0,0 +1,22 @@
+package pe.edu.upeu.sysventas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "upeu_unid_medida")
+public class UnidadMedida {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUnidad;
+
+    @Column( nullable = false, length = 60)
+    private String nombreMedida;
+}
